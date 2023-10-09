69d9f2fd9a1b717664aa3de803bc552a179d6483
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-09 13:51:41 +0300

    Made finding radius little bit bigger

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -34,7 +34,7 @@ const Skill: FC<skillProps> = ({ children, vector, cursor, setFoundSkillsCount }
 
   const animate = () => {
     // Cursor Hover
-    const findingRadius = 50
+    const findingRadius = 80
 
     // Check position of a SKILL in relation to CURSOR
     const isSkillOnTheRight = skillXCenter.current > cursor.x.current - findingRadius
