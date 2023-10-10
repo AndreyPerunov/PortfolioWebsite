c9c4ebe3f229d738be707dcf99a070f43c2034e4
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-10-10 19:58:42 +0300

    Edited footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,8 +4,13 @@ import { FC } from "react"
 const Footer: FC = () => {
   return (
     <div className={styles.footer}>
-      <p>The footer will be here.</p>
-      <small>2023</small>
+      <p>
+        Contact me:{" "}
+        <a className={styles.footer__link} href="mailto:andrey.perunov1@gmail.com">
+          andrey.perunov1@gmail.com
+        </a>
+      </p>
+      <small style={{ paddingBottom: "1em" }}>2023</small>
     </div>
   )
 }
