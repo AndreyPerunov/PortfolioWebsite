6ee5f74d4c92e8eefe9ab0a7beb1ac88b2b26a16
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-11-08 20:13:29 +0200

    Removed copytext

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,5 +1,4 @@
 import styles from "./modules/Contact.module.scss"
-import CopyText from "./CopyText"
 import ContactForm from "./ContactForm"
 import { HiOutlineMail, HiPhone, HiLocationMarker } from "react-icons/hi"
 
@@ -15,12 +14,12 @@ const Contact = () => {
         <div>
           <HiPhone className={styles.contact__info__icon} />
           <h2>Phone</h2>
-          <CopyText content="+371 27 801 043" toClipboard="+37127801043" />
+          <p>+371 27 801 043</p>
         </div>
         <div>
           <HiOutlineMail className={styles.contact__info__icon} />
           <h2>Email</h2>
-          <CopyText content="andrey.perunov1@gmail.com" />
+          <p>andrey.perunov1@gmail.com</p>
         </div>
       </div>
       <div className={styles.contact__form}>
