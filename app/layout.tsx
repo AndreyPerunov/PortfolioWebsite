d9b14371deca5129e0d65f6ddb6ecfc203cb6d3e
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2023-11-10 16:25:19 +0200

    improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,15 +18,30 @@ const allerta = Allerta({
 })
 
 export const metadata = {
+  applicationName: "Portfolio Website",
   title: {
-    default: "Andrey",
+    default: "Andrey | Portfolio Website",
     template: "%s | Andrey"
   },
   icons: {
     icon: "/favicon.ico"
   },
-  description: "My name is Andrey Perunov, I am a professional web developer.",
-  keywords: ["Andrey Perunov", "Perunov", "Andrey", "Frontend", "Backend", "Fullstack", "JavaScript", "React", "..."]
+  description: "My name is Andrey Perunov, I am a professional web developer. This is portfolio website. Here you can find my projects, skills and other information about me",
+  keywords: ["Andrey Perunov", "Perunov", "Andrey", "Frontend", "Backend", "Fullstack", "JavaScript", "React", "Portfolio", "Riga", "Latvia", "Web Developer"],
+  creator: "Andrey Perunov",
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1
+    }
+  }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
