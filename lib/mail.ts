aa165d2bb2f1498c1fad019f6a3579be0079decd
AndreyPerunov <104921155+AndreyPerunov@users.noreply.github.com>
2024-10-05 19:43:39 +0300

    awaited html render

--- lib/mail.ts
+++ lib/mail.ts
@@ -8,24 +8,30 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 type result = { success?: string; error?: string }
 
 export const sendMail = async (name: string, from: string, body: string, time: string): Promise<result> => {
+  // Render HTML
+  const htmlToMe = await render(ToMe({ name, from, body, time }))
+
   // Send email to me
   const res1 = await resend.emails.send({
     from: "form@andreyperunov.com",
     to: "andrey.perunov1@gmail.com",
     subject: `Form Message, from ${name}`,
-    html: render(ToMe({ name, from, body, time }))
+    html: htmlToMe
   })
 
   if (res1.error) {
     return { error: "Error sending email" }
   }
 
+  // Render HTML
+  const htmlRespond = await render(Respond({ name }))
+
   // Send response to user email
   const res2 = await resend.emails.send({
     from: "form@andreyperunov.com",
     to: from,
     subject: "Thank you for contacting me!",
-    html: render(Respond({ name }))
+    html: htmlRespond
   })
 
   if (res2.error) {
